0fd62cf5ee30c0fc0673f55230342245bf66c2c5
Richard <richardstuart007@hotmail.com>
2022-11-14 22:46:55 +1300

    ports

--- quizServerConstants.js
+++ quizServerConstants.js
@@ -7,7 +7,17 @@ exports.REMOTE_KNEX_HOST = 'containers-us-west-71.railway.app'
 exports.REMOTE_KNEX_USER = 'postgres'
 exports.REMOTE_KNEX_PWD = 'cfMC0QKSa86EIbpBdqfk'
 exports.REMOTE_KNEX_DATABASE = 'railway'
-exports.REMOTE_URL_PORT = 9001
+exports.REMOTE_URL_PORT = 59001
+//
+//  Database (Railway 20)
+//
+exports.R20R_KNEX_CLIENT = 'pg'
+exports.R20R_KNEX_PORT = 7306
+exports.R20R_KNEX_HOST = 'containers-us-west-117.railway.app'
+exports.R20R_KNEX_USER = 'postgres'
+exports.R20R_KNEX_PWD = 'xw85A7Dol3dpWGDOYdC1'
+exports.R20R_KNEX_DATABASE = 'railway'
+exports.R20R_URL_PORT = 59002
 //
 //  Database (Local)
 //
@@ -16,7 +26,7 @@ exports.LOCAL_KNEX_HOST = '127.0.0.1'
 exports.LOCAL_KNEX_USER = 'richa'
 exports.LOCAL_KNEX_PWD = 'london'
 exports.LOCAL_KNEX_DATABASE = 'Quiz020'
-exports.LOCAL_URL_PORT = 8001
+exports.LOCAL_URL_PORT = 58001
 //
 //  URL
 //
